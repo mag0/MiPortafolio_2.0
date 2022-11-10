0c5b147f941e87f0b82bcffdcd8c4a86f10c5f47
martin <guerreiromartin@gmail.com>
2022-11-10 00:29:55 -0300

    mi portafolio 2.0

---
+++ js/script.js
@@ -0,0 +1,55 @@
+const nav  = document.querySelector('.main');
+const button_nav  = document.querySelector('.submenu');
+
+button_nav.addEventListener('click', ()=>{
+    nav.classList.toggle('open')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// window.sr = ScrollReveal();
+
+//     sr.reveal('.navbar', {
+//         duration: 3000,
+//         origin: 'bottom',
+//         distance: '-100px'
+//     });
+
+    
+// window.sr = ScrollReveal();
+
+//     sr.reveal('.navbar', {
+//         duration: 3000,
+//         origin: 'bottom',
+//         distance: '-100px'
+//     });
