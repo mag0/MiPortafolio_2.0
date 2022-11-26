68cd1cb1851c85d6353acc9df34284a5caf78e87
martin <guerreiromartin@gmail.com>
2022-11-25 22:25:36 -0300

    validacion de formulario y agregado de proyectos

--- js/script.js
+++ js/script.js
@@ -1,10 +1,35 @@
+
+// MAIN
+
 const nav  = document.querySelector('.menu');
-var section  = document.querySelectorAll('.section');
 const button_nav  = document.querySelector('.submenu');
+
 
 button_nav.addEventListener('click', ()=>{
     nav.classList.toggle('open');
 })
+
+
+// FORMULARIO
+    const names = document.querySelector('.name');
+    const surname = document.querySelector('.surname');
+    const email = document.querySelector('.email');
+    const subject = document.querySelector('.subject');
+    const message = document.querySelector('.message');
+    const submit = document.querySelector('.submit');
+    const confirm = document.querySelector('.confirm');
+    
+    submit.addEventListener('click', e=>{
+        e.preventDefault()
+        if(names.value.length >= 3 && surname.value.length >= 3 
+            && email.value.length >= 12 && subject.value.length >= 1 
+            && message.value.length > 1){
+            confirm.textContent = 'Gracias por escribirme, pronto te contestare';
+        }
+        else{
+            confirm.textContent = '¡Por favor, completa todos los campos!'
+        }
+    })
 
 
 
